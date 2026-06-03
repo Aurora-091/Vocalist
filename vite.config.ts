4eaa9ee552804abab68805965479fd205bdb5b78
Rushikesh Pawar <125253603+I-invincib1e@users.noreply.github.com>
2026-06-03 15:03:56 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      "/v1": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+      "/webhooks": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
   build: { outDir: "dist", sourcemap: false },
 });
