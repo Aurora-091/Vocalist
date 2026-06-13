e741cb00659fbe466fe5f2e2db2e61c0c3934869
Rushikesh Pawar <125253603+I-invincib1e@users.noreply.github.com>
2026-06-13 17:54:10 +0530

    Added vite-env.d.ts

--- public/src/components/ui/select.tsx
+++ public/src/components/ui/select.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { Select as SelectPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
-import { ChevronDown as ChevronDownIcon, Check as CheckIcon, ChevronUp as ChevronUpIcon } from "lucide-react"
+import { ChevronDownIcon, CheckIcon, ChevronUpIcon } from "lucide-react"
 
 function Select({
   ...props
