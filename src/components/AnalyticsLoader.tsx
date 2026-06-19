894103d7a3997d2e8bab2038778a85cb9d1c5090
neore <neore@example.com>
2026-06-19 13:19:33 +0530

    fix: format GTM script snippet and adjust head/body injection positions

--- src/components/AnalyticsLoader.tsx
+++ src/components/AnalyticsLoader.tsx
@@ -54,21 +54,15 @@ export function AnalyticsLoader() {
       const script = document.createElement("script");
       script.id = "gtm-script";
       script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${gtmId}');`;
-      document.head.appendChild(script);
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`;
+      document.head.insertBefore(script, document.head.firstChild);
 
       const noscript = document.createElement("noscript");
       noscript.id = "gtm-noscript";
-      const iframe = document.createElement("iframe");
-      iframe.src = `https://www.googletagmanager.com/ns.html?id=${gtmId}`;
-      iframe.height = "0";
-      iframe.width = "0";
-      iframe.style.display = "none";
-      iframe.style.visibility = "hidden";
-      noscript.appendChild(iframe);
+      noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
       document.body.insertBefore(noscript, document.body.firstChild);
 
       gtmInjected = true;
